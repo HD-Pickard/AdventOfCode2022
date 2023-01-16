5ca8a561af16a49161c5eb2924cf492dff087cf3
Harrison Pickard <HPickard@ematics.co.uk>
2023-01-16 09:25:59 +0000

    Add project files.

---
+++ Tests/Day1Tests.cs
@@ -0,0 +1,16 @@
+namespace Tests
+{
+    public class Day1Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
